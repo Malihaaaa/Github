a7811908ef92eb2af59844d3adfee0d1814b8c1b
hibahariiri <78786591+hibahariiri@users.noreply.github.com>
2021-02-18 11:15:03 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here Hej Maliha
 	    int hej = 12;
+	    
+	    system.out.println("10 + 2 er: " + hej");	     
     }
 }
